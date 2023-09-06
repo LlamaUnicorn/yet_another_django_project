a10b4949cf4afdaeb168b33dcda489256a75930b
LlamaUnicorn <holidaynight@gmail.com>
2023-09-06 06:51:10 +0300

    04-06 temlates-filters

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -37,8 +36,10 @@ def index(request):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported</h1>")
 
