fe37bdee6a9d125c7cc585896c6dbc6ae7a90f70
LlamaUnicorn <holidaynight@gmail.com>
2023-09-12 07:02:11 +0300

    04-16

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
@@ -40,7 +40,7 @@ def monthly_challenge(request, month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        raise Http404()
 
 
 def monthly_challenge_by_number(request, month):
