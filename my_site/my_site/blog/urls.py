131404f2131d0fd85bbce15eea57b3e91708f774
LlamaUnicorn <holidaynight@gmail.com>
2023-09-21 06:44:42 +0300

    05-06

--- my_site/my_site/blog/urls.py
+++ my_site/my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view.starting_page, name="starting_page"),
+    path("", views.starting_page, name="starting-page"),
     path("posts/", views.posts, name="posts-page"),
     path("posts/<slug>", views.post_detail, name="post-detail-page")
 ]
